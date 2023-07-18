41a4e0af778060a1b09ad5d373ed4fe1c3010c01
SirGraybon <graydonritchie@gmail.com>
2023-07-18 13:18:20 -0400

    allows adding of dice to bowl

--- src/components/DiceBowl.jsx
+++ src/components/DiceBowl.jsx
@@ -1,17 +1,60 @@
+import { useState } from "react";
 import "../styles/diceBowl.css";
 
 const DiceBowl = function (props) {
+  const [dice, setDice] = useState([]);
+
+  const addDie = function (d) {
+    let keyCounter = dice.length;
+    const currentDice = [...dice];
+    currentDice.push({
+      die_key: keyCounter,
+      die_type: d,
+      result: 0,
+      status: "not_rolled",
+    });
+
+    setDice(currentDice);
+  };
+
+  const rollDie = function (index) {
+    const currentDice = [...dice];
+    let targetDie = currentDice[index];
+    targetDie.result = Math.floor(Math.random() * targetDie.die_type) + 1;
+    targetDie.status = "rolled";
+    setDice(currentDice);
+  };
   return (
     <div className="diceBowlComponent">
       <div className="dice_options">
-        <div className="dice_option"></div>
-        <div className="dice_option"></div>
-        <div className="dice_option"></div>
-        <div className="dice_option"></div>
-        <div className="dice_option"></div>
-        <div className="dice_option"></div>
+        <div className="dice_option" onClick={() => addDie(20)}>
+          20
+        </div>
+        <div className="dice_option">12</div>
+        <div className="dice_option">10</div>
+        <div className="dice_option">8</div>
+        <div className="dice_option">6</div>
+        <div className="dice_option">4</div>
       </div>
-      <div className="dice_roll_area"></div>
+      <div className="dice_roll_area">
+        {dice.map((die, index) => {
+          return (
+            <div className={die.status} key={die.die_key}>
+              <div className="die_display_area">
+                <p>D{die.die_type}</p>
+                <p>{die.result}</p>
+              </div>
+              <div className="die_actions">
+
+              <a>X</a>
+              <div className="roll" onClick={() => rollDie(index)}>
+                Roll
+              </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };

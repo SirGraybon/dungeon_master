f4a393a54f16905a3c90cb90fdb9a9d8b9403184
SirGraybon <graydonritchie@gmail.com>
2023-07-21 19:38:54 -0400

    allows on the fly terrain editing

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -47,7 +47,7 @@ export default function Board() {
                         </Draggable>
                       );
                     })}
-                    {provided.placeholder}
+                    {/* {provided.placeholder} */}
                   </div>
                 )}
               </Droppable>
--- src/components/InformationCenter.jsx
+++ src/components/InformationCenter.jsx
@@ -43,7 +43,7 @@ export default function InformationCenter() {
                       )}
                     </Draggable>
 
-                    {provided.placeholder}
+                    {/* {provided.placeholder} */}
                   </div>
                 )}
               </Droppable>
